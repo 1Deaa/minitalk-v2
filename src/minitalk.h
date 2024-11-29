539b869f4781a6a69c113b2b8e36279f856aa9a7
drahwanj <deaarahwnji@gmail.com>
2024-11-29 13:37:28 +0300

    Added ft_printf header

--- src/minitalk.h
+++ src/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: drahwanj <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 16:56:07 by drahwanj          #+#    #+#             */
-/*   Updated: 2024/11/24 17:52:09 by drahwanj         ###   ########.fr       */
+/*   Updated: 2024/11/29 13:21:58 by drahwanj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 # include <signal.h>
 # include <unistd.h>
 # include <stdlib.h>
+# include "../ft_printf/ft_printf.h"
 
 int		ft_atoi(const char *str);
 void	_kill(pid_t pid, int signo);
